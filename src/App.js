02ad0dfbacd9b37ec2aa5bde0a2462285129cbb2
Sergey Solodukhin <soloss@yandex.ru>
2019-09-15 15:29:08 +0300

    play with CSSTransition

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React from 'react'
-import { CssBaseline, Button } from '@material-ui/core'
-import './App.css'
+import React, { useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
+import { CssBaseline, Button, Box, Card, List, ListItem, ListItemText } from '@material-ui/core'
+
+import useStyles from './styles'
 
 function App() {
+  const [openList, setOpenList] = useState(false)
+  const classes = useStyles()
+
+  const toggleList = () => setOpenList(!openList)
+
   return (
     <React.Fragment>
       <CssBaseline />
-      <div className='App'>
-        <header className='App-header'>
-          <Button variant='contained' color='primary'>
-            Hello World
-          </Button>
-        </header>
-      </div>
+      <Box m={2}>
+        <Button variant='contained' color='primary' onClick={toggleList}>
+          List with CSSTransition
+        </Button>
+        <CSSTransition
+          in={openList}
+          timeout={400}
+          classNames={{
+            enter: classes.listTransitionEnter,
+            enterActive: classes.listTransitionEnterActive,
+            exit: classes.listTransitionExit,
+            exitActive: classes.listTransitionExitActive
+          }}
+          unmountOnExit
+        >
+          <Card>
+            <List>
+              <ListItem>
+                <ListItemText primary='Feed the dog' />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary='Cut hair' />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary='Do the dishs' />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary='Buy grossries' />
+              </ListItem>
+            </List>
+          </Card>
+        </CSSTransition>
+      </Box>
     </React.Fragment>
   )
 }
---
+++ src/styles.js
@@ -0,0 +1,27 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  listTransitionEnter: {
+    opacity: 0,
+    transform: 'scale(0.1)'
+  },
+
+  listTransitionEnterActive: {
+    opacity: 1,
+    transform: 'scale(1.0)',
+    transition: 'all 400ms'
+  },
+
+  listTransitionExit: {
+    opacity: 1,
+    transform: 'scale(1.0)'
+  },
+
+  listTransitionExitActive: {
+    opacity: 0,
+    transform: 'scale(0.1)',
+    transition: 'all 400ms'
+  }
+}))
+
+export default useStyles
